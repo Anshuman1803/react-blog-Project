dd0458b08a03c47830089ff745350677c9c5e714
Anshuman Kumar <mahianshuman491@gmail.com>
2023-10-10 00:58:10 +0530

    Home-Section posterUpdate

--- src/Components/BlogHome.js
+++ src/Components/BlogHome.js
@@ -3,8 +3,21 @@ import React from 'react'
 function BlogHome() {
   return (
     <section className='blogSection'>
-    HOME
-  </section>
+
+      <div className="Hero-Section">
+        <div className="poster poster-1">
+          <img src="https://assets-in.bmscdn.com/discovery-catalog/events/et00025234-ldmsvbjwpl-landscape.jpg" alt="poster" className='posterImage' />
+        </div>
+        <div className="poster poster-2">
+          <img src='https://img.freepik.com/premium-vector/concept-machine-learning-innovation-technology-graphic-ai-head-with-programming-script_46706-1054.jpg' alt="poster" className='posterImage' />
+        </div>
+        <div className="poster poster-3">
+          <img src="https://m.media-amazon.com/images/I/61RuvWDxo8L._AC_UF1000,1000_QL80_.jpg" alt="poster" className='posterImage' />
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 
