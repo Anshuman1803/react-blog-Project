1ee48993603172d6c57ee24a6710db418a63ad44
Anshuman Kumar <mahianshuman491@gmail.com>
2023-10-09 19:27:50 +0530

    Nav and Icon Update

--- src/Components/Page404.js
+++ src/Components/Page404.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useNavigate} from 'react-router-dom'
 function Page404() {
+  let navigateToHome = useNavigate();
   return (
     <div className='Page404'>
-      <button className='goHomeBtn'>Go Home</button>
+      <button className='goHomeBtn' onClick={(e)=>navigateToHome('/')}>Go Home</button>
     </div>
   )
 }
